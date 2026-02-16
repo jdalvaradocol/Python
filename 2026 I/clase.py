508e9f8a8b1c87f4e6b97e785265a5c4372b57e5
Jose David <80493134+jdalvaradocol@users.noreply.github.com>
2026-02-16 08:30:45 -0500

    Create clase.py

---
+++ 2026 I/clase.py
@@ -0,0 +1,78 @@
+
+print("Bienvenido a la clase de Python A \n")
+print('Bienvenido a la clase de Python B\n')
+
+## Tipos de variables en Python
+
+# Variables de tipo entero
+
+# Declaración de una variable entera
+
+variable = 10
+print("La variable entera es:", variable)
+print("El tipo de variable_entera es:", type(variable))
+
+# Declaración de una variable flotante   
+
+variable = 10.5
+print("La variable flotante es:", variable)
+print("El tipo de variable_flotante es:", type(variable))
+
+# Declaración de una variable cadena de texto  
+
+variable = 'Hola Mundo'
+print("La variable cadena de texto es:", variable)
+print("El tipo de variable_cadena es:", type(variable))
+
+## Operacaiones en Python.
+
+# Suma
+
+a = 80
+b = 50
+suma = a + b
+print("La suma de a y b es:", suma)
+print("El tipo de variable:", type(suma))
+    
+# Resta
+
+a = 80
+b = 50
+resta = a - b
+print("La resta de a y b es:", resta)
+print("El tipo de variable:", type(resta))
+
+# multiplicación
+
+a = 80
+b = 50
+multiplicacion = a * b
+print("La multiplicación de a y b es:", multiplicacion)
+print("El tipo de variable:", type(multiplicacion))
+
+# División
+a = 80
+b = 50          
+division = a / b
+print("La división de a y b es:", division)
+print("El tipo de variable:", type(division))
+
+a = 80
+b = 50          
+division = int(a / b)
+print("La división de a y b es:", division)
+print("El tipo de variable:", type(division))
+
+a = 80
+b = 50          
+division = a // b
+print("La división de a y b es:", division)
+print("El tipo de variable:", type(division))
+
+# Modulo.
+a = 80
+b = 50          
+
+modulo = a % b
+print("El módulo de a y b es:", modulo) 
+print("El tipo de variable:", type(modulo))
